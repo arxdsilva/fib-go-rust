d515dcee24c6c791ba523607de4f1c78d1b973bb
Arthur Silva <arxdsilva@gmail.com>
2019-11-11 08:51:16 -0300

    add sequential fibonacci

--- go/main_test.go
+++ go/main_test.go
@@ -20,3 +20,18 @@ func BenchmarkFib3k(b *testing.B)  { benchmarkFib(3000, b) }
 func BenchmarkFib10k(b *testing.B) { benchmarkFib(10000, b) }
 func BenchmarkFib20k(b *testing.B) { benchmarkFib(20000, b) }
 func BenchmarkFib40k(b *testing.B) { benchmarkFib(40000, b) }
+
+func benchmarkFibSeq(fibN uint, b *testing.B) {
+	b.ReportAllocs()
+	for i := 2; i < b.N; i++ {
+		var wg sync.WaitGroup
+		wg.Add(1)
+		FibonacciBig(fibN, &wg)
+	}
+}
+
+func BenchmarkFibSeq2k(b *testing.B)  { benchmarkFibSeq(2000, b) }
+func BenchmarkFibSeq3k(b *testing.B)  { benchmarkFibSeq(3000, b) }
+func BenchmarkFibSeq10k(b *testing.B) { benchmarkFibSeq(10000, b) }
+func BenchmarkFibSeq20k(b *testing.B) { benchmarkFibSeq(20000, b) }
+func BenchmarkFibSeq40k(b *testing.B) { benchmarkFibSeq(40000, b) }
