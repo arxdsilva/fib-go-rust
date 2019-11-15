6e03f15458f6a612677af0456268b24e6b7b644b
Arthur Silva <arxdsilva@gmail.com>
2019-11-15 12:30:26 -0300

    add initial bench test

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -63,16 +63,42 @@ fn main() {
 
 fn run_fib(n: usize, size: usize) {
     task::block_on(async {
-        let tasks = (0..size).into_iter().map(|_| task::spawn(fib(n)));
-        let results = futures::future::join_all(tasks).await;
+        let tasks = (0..size).into_iter().map(|BigUint| task::spawn(fib(n)));
+        futures::future::join_all(tasks).await;
     })
 }
 
-async fn fib(n: usize) {
+async fn fib(n: usize) -> BigUint {
     let mut f0: BigUint = Zero::zero();
     let mut f1: BigUint = One::one();
     for _ in 0..n {
         let f2 = f0 + &f1;
         f0 = replace(&mut f1, f2);
     }
+    f0
 }
+
+// old implementation
+// fn main2() {
+//     let start = SystemTime::now();
+//     fib_thread(2000);
+//     let end = SystemTime::now();
+//     let duration = end.duration_since(start)
+//         .expect("Time went backwards");
+//     let ns = duration.as_nanos();
+//     println!("fib_2k    {} ns/op", ns/2000);
+// }
+
+// fn fib_thread(n: usize) {
+//     let (tx, rx) = mpsc::channel();
+//     for _ in 0..2000 {
+//         let tx = tx.clone();
+//         thread::spawn(move || {
+//             fib(n);
+//             tx.send(());
+//         });
+//     }
+//     for _ in 0..2000 {
+//         rx.recv().ok();
+//     }
+// }
